3e3d6cf0e38789dd95cdc8b3d9379474a40b7e46
Filippo Ranza <filipporanza@gmail.com>
2018-11-19 21:29:48 +0100

    now multiple ||(abs) and/or \|\|(norm) on one line are parsed correctly

--- fast_patch.pl
+++ fast_patch.pl
@@ -3,7 +3,7 @@
 #
 #   fast_patch.pl - Automatically refactor Latex code
 #
-#   Copyright (c) 2009 Filippo Ranza <filipporanza@gmail.com>
+#   Copyright (c) 2018 Filippo Ranza <filipporanza@gmail.com>
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -37,7 +37,7 @@ sub load_marco{
             if(/^$/ or /^#/){
                 next;
             }
-        
+
             if (/^(.+)\s+'(.+)'$/ or /^(.+)\s+"(.+)"$/){
                 $out{$1} = $2;
             }
@@ -58,21 +58,21 @@ sub patch{
     my %macros = load_marco;
     my $ps = 0;
     while(<$in>){
-    
+
         $ps = 1 if(/\\begin\{align|equation(.+)?\}/);
         $ps = 0 if(/\\end\{align|equation(.+)?\}/);
         if($ps){
             s|(\\left)?\(|\\left\(|g;
             s|(\\right)?\)|\\right\)|g;
-            
+
             s|(\\left)?\[|\\left\[|g;
             s|(\\right)?\]|\\right\]|g;
-            
-            s/\\\|(.+)\\\|/\\norm\{$1\}/g;
-            s/\|(.+)\|/\\abs\{$1\}/g;
-            
+
+            s/\\\|([^\|]+)\\\|/\\norm\{$1\}/g;
+            s/\|([^\|]+)\|/\\abs\{$1\}/g;
+
         }
-        
+
         for my $k (keys %macros){
             s|$k|$macros{$k}|g;
         }
@@ -85,12 +85,12 @@ sub run_patch{
     (my $in_name) = @_;
     open my $in, $in_name || die "$!";
     (my $out, my $out_name) = tempfile();
-    
+
     patch $in, $out;
-    
+
     close $in;
     close $out;
-    
+
     move $in_name, "$in_name.bak" || print $!;
     move $out_name, "$in_name" || print $!;
 
@@ -101,8 +101,3 @@ sub run_patch{
 foreach my $arg (@ARGV){
     run_patch $arg;
 }
-
-
-
-
-
